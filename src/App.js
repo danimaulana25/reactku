d3ff4eb4f25eb3cb57619c05cec72c922f87cc99
danimaulana25 <ahmaddanimaulana80@gmail.com>
2023-03-21 00:31:50 +0700

    Kuis 1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from "react";
-import Header from './Header';
-import Footer from "./Footer";
-import List from './List';
+import Mahasiswa from "./container/Mahasiswa/Mahasiswa";
+import Footer from "./pages/Footer";
+import Header from "./pages/Header";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
-        <h1>Component dari Class App</h1>
-        <List/>
-        <Footer judul='Halaman Footer' nama='Aufa'/>
-      </div>
+        <Header />
+        <Mahasiswa />
+        <Footer />
+      </div >
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-// import Test from './Test'
 // import App from './App';
-import Tugas from './Tugas/Tugas';
-import HelloComponent from './component/HelloComponent';
 import reportWebVitals from './reportWebVitals';
+// import BlogPost from './container/BlogPost/BlogPost';
+import Mahasiswa from './container/Mahasiswa/Mahasiswa';
 
-// const HelloComponent = () =>{
-//   return HelloComponent
-// }
 
-// class StateFullComponent extends React.Component{
-//   render(){
-//     return<p>StateFullComponent</p>
-//   }
-// }
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('content'));
 root.render(
   <React.StrictMode>
-    <Tugas />
+    <Mahasiswa />
   </React.StrictMode>
-);
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
